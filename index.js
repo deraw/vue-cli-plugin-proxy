ba34bcc19a5b1e5ec2797ea396f47cf1bedd1672
Dylan Broussard <deraw@outlook.fr>
2018-10-02 03:29:05 +0200

    🐛 Fix projectOptions

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var proxy = require('http-proxy-middleware');
 
 module.exports = api => {
 	api.configureDevServer((app) => {
-		app.use(proxy(projectOptions.pluginOptions.proxy.context, projectOptions.pluginOptions.proxy.options));
+		app.use(proxy(
+			api.projectOptions.pluginOptions.proxy.context,
+			api.projectOptions.pluginOptions.proxy.options
+		));
 	});
 };
