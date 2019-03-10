fdad3e28326f4082130fec71dd7fb678569fd932
Clement Yan <yanclement94@gmail.com>
2019-03-10 16:42:40 +0800

    Add option for disabling the proxy

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 var proxy = require('http-proxy-middleware');
 
 module.exports = (api, options) => {
-	api.configureDevServer((app) => {
-		app.use(proxy(
-			options.pluginOptions.proxy.context,
-			options.pluginOptions.proxy.options
-		));
-	});
+	const opts = options.pluginOptions.proxy
+	if(opts && (opts.enabled === undefined || opts.enabled)) {
+		api.configureDevServer((app) => {
+			app.use(proxy(
+				opts.context,
+				opts.options
+			));
+		});
+	}
 };
