343fb9ebdff4da8f67dd576202e9f4adaef28508
Dylan Broussard <deraw@outlook.fr>
2019-03-11 14:49:32 +0100

    🎨 Improve format of the code

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 var proxy = require('http-proxy-middleware');
 
 module.exports = (api, options) => {
-	const opts = options.pluginOptions.proxy
-	if(opts && (opts.enabled === undefined || opts.enabled)) {
+	const opts = options.pluginOptions.proxy;
+
+	if (opts && (opts.enabled === undefined || opts.enabled)) {
 		api.configureDevServer((app) => {
 			app.use(proxy(
 				opts.context,
