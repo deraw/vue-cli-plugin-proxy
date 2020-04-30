e20e9ac3a3026ea080f99f5bcbb6d9fc0e41bb8f
Dylan Broussard <deraw@outlook.fr>
2020-04-30 11:53:42 +0200

    ♻️ Update http-proxy-middleware import

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const proxy = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (api, options) => {
 	const opts = options.pluginOptions && options.pluginOptions.proxy;
 
 	if (opts && (opts.enabled === undefined || opts.enabled)) {
 		api.configureDevServer((app) => {
-			app.use(proxy(
+			app.use(createProxyMiddleware(
 				opts.context,
 				opts.options
 			));
